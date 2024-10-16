bcf113ee975dfc71f36872935176a0c6fb64ccac
theblaze98 <dionijvargas@gmail.com>
2024-10-16 10:13:03 -0400

    feat: :sparkles: add validation for expire verify code exeption

--- server/src/helpers/http-error-validation.ts
+++ server/src/helpers/http-error-validation.ts
@@ -5,10 +5,12 @@ export const httpErrorValidation = (
   statusCode: number,
 ) => {
   const matcher: { [key: string]: { message: string; statusCode: number } } = {
-    USER_ALREADY_EXISTS: { message: errorMessage, statusCode },
+    EMAIL_ALREADY_EXISTS: { message: errorMessage, statusCode },
+    USERNAME_ALREADY_EXISTS: { message: errorMessage, statusCode },
     USER_NOT_FOUND: { message: errorMessage, statusCode },
     INVALID_CREDENTIALS: { message: errorMessage, statusCode },
     INVALID_VERIFY_CODE: { message: errorMessage, statusCode },
+    EXPIRE_VERIFY_CODE: { message: errorMessage, statusCode },
   }
 
   const defaultError = {
