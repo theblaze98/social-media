07a6a35cbfa22fd2c45a7bb44c3c33c7acd5937c
theblaze98 <dionijvargas@gmail.com>
2024-10-16 10:11:48 -0400

    feat: :sparkles: update connection with neon database

--- server/src/drizzle/drizzle.module.ts
+++ server/src/drizzle/drizzle.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common'
-import { drizzle } from 'drizzle-orm/node-postgres'
-import { postgresConnection } from './helpers/connection'
+import { drizzle } from 'drizzle-orm/neon-http'
+import { neonPostgressConnection } from './helpers/connection'
 import { DRIZZLE_PROVIDER } from '@/helpers'
+import * as schema from '@/drizzle/schemas'
 
 @Module({
   providers: [
     {
       provide: DRIZZLE_PROVIDER,
       useFactory: async () => {
-        return drizzle(await postgresConnection())
+        try {
+          return drizzle(neonPostgressConnection(), { schema })
+        } catch (error) {
+          console.log(error)
+        }
       },
     },
   ],
+  exports: [DRIZZLE_PROVIDER],
 })
 export class DrizzleModule {}
