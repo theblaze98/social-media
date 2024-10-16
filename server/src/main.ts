5256ab4eaedb88515f96e1185f85da6e37256717
theblaze98 <dionijvargas@gmail.com>
2024-10-16 10:13:47 -0400

    feat: :sparkles: remoce swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -11,14 +10,6 @@ async function bootstrap() {
     origin: '*',
     methods: ['GET', 'POST,', 'PUT', 'PATCH', 'DELETE'],
   })
-  const config = new DocumentBuilder()
-    .setTitle('SpeakFlow Api')
-    .setDescription('')
-    .setVersion('1.0')
-    .build()
-
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('doc', app, document)
 
   await app.listen(Number(process.env.PORT) || 4000)
 }
